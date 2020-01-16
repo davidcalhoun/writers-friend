29d72128050ed51fabde9d7823d8ea53a1473c37
David Calhoun <david.b.calhoun@gmail.com>
2020-01-16 13:13:43 -0500

    WIP file browser

--- src/components/FileBrowser/index.js
+++ src/components/FileBrowser/index.js
@@ -1,32 +1,123 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import styles from "./index.css";
 import uuidv4 from "uuid/v4";
 
-import { getFile, setFile, getLS, setLS } from "../../utils";
+import {
+	getFile,
+	setFile,
+	getLS,
+	setLS,
+	getFileMeta,
+	setFileMeta
+} from "../../utils";
 
 const initialValue = [
-  {
-    type: "paragraph",
-    children: [
-      { text: "This is editable " },
-      { text: "rich", bold: true },
-      { text: " text, " },
-      { text: "much", italic: true },
-      { text: " better than a " },
-      { text: "<textarea>", code: true },
-      { text: "!" }
-    ]
-  }
+	{
+		type: "paragraph",
+		children: [
+			{ text: "This is editable " },
+			{ text: "rich", bold: true },
+			{ text: " text, " },
+			{ text: "much", italic: true },
+			{ text: " better than a " },
+			{ text: "<textarea>", code: true },
+			{ text: "!" }
+		]
+	}
 ];
 
+function Filename(props) {
+	const { isSelected, isEditing, id, name, onFileSelect, onNameChange, onEditBlur } = props;
+
+	const className = `${styles.file} ${
+		isSelected ? styles.fileSelected : ""
+	}`;
+
+	if (isEditing) {
+		return (
+			<li key={id}>
+				<FilenameInput
+					name={name}
+					id={id}
+					onNameChange={onNameChange}
+					onEditBlur={onEditBlur}
+				/>
+			</li>
+		);
+	}
+
+	return (
+		<li
+			className={className}
+			onClick={onFileSelect.bind(null, id)}
+		>
+			{name}
+		</li>
+	);
+}
+
+function FilenameInput(props) {
+	const [value, setValue] = useState("");
+	const inputEl = useRef(null);
+
+	const { id, name, onNameChange, onEditBlur } = props;
+
+	useEffect(() => {
+		setValue(name);
+
+		inputEl.current.focus();
+	}, []);
+
+	function handleChange(event) {
+		setValue(event.target.value);
+	}
+
+	function handleNameChangeSubmit(event) {
+		event.preventDefault();
+
+		onNameChange(id, value);
+	}
+
+	function handleFilenameKeyDown(event) {
+		const escapeKey = event.keyCode === 27;
+
+		if (escapeKey) {
+			onEditBlur(id);
+		}
+	}
+
+	return (
+		<form onSubmit={handleNameChangeSubmit}>
+			<input
+				ref={inputEl}
+				type="text"
+				onChange={handleChange}
+				onBlur={onEditBlur.bind(null, id)}
+				onKeyDown={handleFilenameKeyDown}
+				value={value}
+				className={styles.fileEditInput}
+			/>
+		</form>
+	);
+}
 
 export default function(props) {
 	const [isExpanded, setIsExpanded] = useState(true);
 	const [files, setFiles] = useState([]);
+	const [selectedFile, setSelectedFile] = useState("");
+	const [nameEdit, setNameEdit] = useState({});
 	const { onFileSelect } = props;
 
 	useEffect(() => {
+		const files = getFilesLS();
+		setFiles(files);
+
+		// Open the first file.
+		handleFileSelect(files[0].id);
+	}, []);
+
+	function getFilesLS() {
 		let allFiles = getLS("files");
 		if (!allFiles) {
 			allFiles = initLocalStorage();
@@ -36,11 +127,8 @@ export default function(props) {
 			return;
 		}
 
-		setFiles(allFiles);
-
-		// Open the first file.
-		onFileSelect(allFiles[0].id);
-	}, []);
+		return allFiles;
+	}
 
 	function handleToggleExpand() {
 		setIsExpanded(!isExpanded);
@@ -90,6 +178,41 @@ export default function(props) {
 		onFileSelect(createdId);
 	}
 
+	function handleFileSelect(id) {
+		if (id === selectedFile) {
+			// Already selected, so enter filename editing mode.
+			const { name } = getFileMeta(id);
+
+			setNameEdit({
+				id,
+				text: name
+			});
+		} else {
+			// Open new file.
+			onFileSelect(id);
+			setSelectedFile(id);
+		}
+	}
+
+	function handleNameChange(id, name) {
+		const oldMeta = getFileMeta(id);
+		const newMeta = {
+			...oldMeta,
+			name
+		};
+
+		setFileMeta(id, newMeta);
+
+		setNameEdit({});
+
+		const files = getFilesLS();
+		setFiles(files);
+	}
+
+	function handleEditBlur(id) {
+		setNameEdit({});
+	}
+
 	let containerClass = styles.container;
 	containerClass +=
 		" " + (isExpanded ? styles.expandOpen : styles.expandClosed);
@@ -104,9 +227,20 @@ export default function(props) {
 					{isExpanded ? "Close" : "Open"}
 				</button>
 			</div>
-			<ul className={styles.browser}>
+			<ul className={styles.files}>
 				{files.map(({ id, name }) => {
-					return <li className={styles.file} onClick={onFileSelect.bind(null, id)} key={id}>{name}</li>;
+					return (
+						<Filename
+							key={id}
+							id={id}
+							name={name}
+							isSelected={selectedFile === id}
+							isEditing={nameEdit.id === id}
+							onFileSelect={handleFileSelect}
+							onNameChange={handleNameChange}
+							onEditBlur={handleEditBlur}
+						/>
+					);
 				})}
 			</ul>
 		</div>
